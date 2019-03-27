50c83318048a8303643b3960bb36c7f8f9f976d8
Yakhin Vadim <yakhin.v@gmail.com>
2019-03-27 14:04:43 -0300

    add basic components

---
+++ src/components/SearchResults/SearchResults.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '../Card/Card'
+
+const SearchResults = () => {
+  return (
+    <div>
+      total: XX
+      <Card/>
+      <Card/>
+      pagination
+    </div>
+  )
+}
+
+export default SearchResults
