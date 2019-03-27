1ce5327965f6be748ea63bc5d53591f8fe7d6e19
Yakhin Vadim <yakhin.v@gmail.com>
2019-03-27 14:47:38 -0300

    remove redundant css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import SearchForm from "./components/SearchForm/SearchForm";
 import SearchResults from "./components/SearchResults/SearchResults";
 import { Incident } from "./types.d";
