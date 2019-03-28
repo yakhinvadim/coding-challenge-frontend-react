9151e1729a45a692392f13eb530535d15c61e6fb
Yakhin Vadim <yakhin.v@gmail.com>
2019-03-28 09:19:40 -0300

    show dates correctly

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Incident } from "../../types.d";
 
+const MILLISECONDS_IN_ONE_SECOND = 1000;
+
 type Props = {
   incident: Incident;
 };
@@ -16,9 +18,15 @@ const Card: React.FunctionComponent<Props> = ({ incident }) => {
       <br />
       description: {incident.description}
       <br />
-      occured at: {incident.occurred_at}
+      occured at:{" "}
+      {new Date(
+        incident.occurred_at * MILLISECONDS_IN_ONE_SECOND
+      ).toLocaleString()}
       <br />
-      updated at: {incident.updated_at}
+      updated at:{" "}
+      {new Date(
+        incident.updated_at * MILLISECONDS_IN_ONE_SECOND
+      ).toLocaleString()}
       <br />
       address: {incident.address}
       <br />
